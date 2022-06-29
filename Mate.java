2192ccd7b02a7d41794fc24d58892ff06b1e9d18
marinacharris <85807129+marinacharris@users.noreply.github.com>
2022-06-28 21:29:57 -0500

    Delete Mate.java

--- Mate.java
+++
@@ -1,9 +0,0 @@
-public class Mate {
-    public static void main(String[] args){
-        System.out.println(Math.min(50, 20));
-        System.out.println(Math.random()); // devuelve un valor aleatorio entre 0 y 1(excluido)
-        System.out.println(Math.random()*100);
-        System.out.println((int)(Math.random()*100));
-    }
-    
-}
